1c7a0855525b89362e0c9eb7cb1a9b805e7359f4
Nicola Borghi <nicoborghi@outlook.it>
2025-11-26 14:27:52 +0100

    Add new meeting details for the "10 Years of Gravitational Waves" lecture and remove outdated meeting link

--- meetings-data.js
+++ meetings-data.js
@@ -22,7 +22,6 @@ const meetingsData = [
         speaker: "Lucia Papalini",
         affiliation: "University of Pisa",
         title: "Overlapping Gravitational-Wave Signals in 3G Detectors: Transformers and Normalizing Flows for joint inference",
-        meetingLink: "https://meet.google.com/dsx-fcga-omu",
         papers: [
             {
                 title: "Can Transformers help us perform parameter estimation of overlapping signals in gravitational wave detectors?",
